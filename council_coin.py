c13f53c8c7c9d8d5eeef784db4ed9e55b48d5c6f
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-05-18 23:01:03 +0800

    Add files via upload

---
+++ council_coin.py
@@ -0,0 +1,29 @@
+# Source: https://codeburst.io/how-to-create-your-own-qr-code-programmatically-7b7290252b15
+
+# pip install qrcode
+# pip install Pillow
+
+import qrcode
+
+def generate_qr_code(studentID):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(studentID)
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="blue", back_color="white")
+    image.save(f'QR_Codes/{studentID}.png')
+
+
+# # Generate one QR code
+# generate_qr_code("kuangwen")
+
+
+userID_list = [12345,23456,'kuangwen']
+
+for userID in userID_list:
+	generate_qr_code(userID)
