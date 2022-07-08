8bb1e79ff37ee807efc9207e9f02110e2e614f38
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2022-07-08 22:19:00 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+# streamlit_app.py
+
+
+# Libraries for connection
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Libraries for QR Code
+from PIL import Image
+import numpy as np
+import cv2
+
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+# @st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+
+# Get all data from the sheet
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+
+# Function to decode QR code
+@st.cache
+def qr_code_dec(image):
+    
+    decoder = cv2.QRCodeDetector()
+    
+    data, vertices, rectified_qr_code = decoder.detectAndDecode(image)
+    
+    if len(data) > 0:
+        print("Decoded Data: '{}'".format(data))
+        
+        rectified_image = np.uint8(rectified_qr_code)
+        
+        decoded_data = data
+        
+        rectified_image = cv2.putText(rectified_image,decoded_data,(50,350),fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale = 2,
+            color = (250,225,100),thickness =  3, lineType=cv2.LINE_AA)
+                
+    return decoded_data
+
+
+# Streamlit stuff
+st.title("Points Tracker")
+st.subheader("Take a photo or upload an image of your QR code to get your points")
+
+
+#uploading the imges
+img_file_buffer = st.file_uploader("Upload an image which you want to Decode", type=[ "jpg", "jpeg",'png'])
+
+if img_file_buffer is not None:
+    image = np.array(Image.open(img_file_buffer))
+
+    student_id = qr_code_dec(image)
+    st.markdown(f"Student ID: **{student_id}**")
+
+    # Initialise state
+    state = "none"
+
+    # Check each row to see if student_id matches, then return the 
+    for row in rows:
+        if student_id == row.student_id:
+            st.markdown(f"Number of Points: **{round(row.number_of_coins)}**")
+            state = "yes"
+            break
+    # If cannot find
+    if state == "none":
+        st.error("Please scan the correct QR code")
+
+
+# References
+# https://medium.com/analytics-vidhya/create-a-qr-code-decoder-web-application-using-opencv-and-streamlit-b0656146e2d1
+# https://docs.streamlit.io/knowledge-base/tutorials/databases/private-gsheet
